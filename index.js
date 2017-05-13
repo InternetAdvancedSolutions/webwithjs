00f87660546d784432d55d03631a3b64ce410848
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-05-13 12:12:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/logout', function(request, response) {
   response.render('pages/logout');
 });
 
+app.get('/answersheet', function(request, response) {
+  response.render('pages/answersheet');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
