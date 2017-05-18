320e782d9bc8033a210beecefa4ea3f8c18fb5b9
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-05-19 00:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.get('/db', function (request, response) {
   });
 });
 
-app.get('/qs', function (request, response) {
+app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM questions', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/qs', {results: result.rows} ); }
+       { response.render('pages/db', {results: result.rows} ); }
     });
   });
 });
