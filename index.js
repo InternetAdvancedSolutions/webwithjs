7cf622e4a1c28e4d85789ad75243fc7a1962df3d
InternetAdvancedSolutions <ashutosh_ks@yahoo.com>
2017-05-19 00:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
+app.get('/qs', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM questions', function(err, result) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
